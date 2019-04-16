9c6cbb094bee71fe982c95ff2d5778f1f30781db
Nelson Brasilio <nbrasilio@hotmail.com>
2019-04-16 20:04:16 -0300

    ATUALIZAÇÃO APÓS RECRIAÇÃO

---
+++ src/application/ProgramTryCatch.java
@@ -0,0 +1,43 @@
+package application;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ProgramTryCatch {
+	public static void main(String[] args) {
+		method1();
+		System.out.println("End of program");
+	}
+
+	public static void method1() {
+		System.out.println("***METHOD1 START***");
+		method2();
+		System.out.println("***METHOD1 END***");
+	}
+
+	public static void method2() {
+		System.out.println("***METHOD2 START***");
+		Scanner sc = new Scanner(System.in);
+		
+		try {
+			String[] vect = sc.nextLine().split(" ");
+			int position = sc.nextInt();
+			System.out.println(vect[position]);
+		} 
+		catch (ArrayIndexOutOfBoundsException e) {
+			System.out.println("Invalid position!");
+			e.printStackTrace();
+			sc.next();
+		} 
+		catch (InputMismatchException e2) {
+			System.out.println("Input error");
+			e2.printStackTrace();
+			sc.next();
+		}
+		finally {
+			sc.close();
+			System.out.println("***METHOD2 END***");
+		}
+		
+	}
+}
